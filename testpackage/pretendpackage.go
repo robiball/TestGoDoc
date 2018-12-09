b7ee0dedf96dbee1a00fe219239f3c8e94022d84
CharlesWinter <charleswinter27@gmail.com>
2018-12-08 17:31:08 -0800

    added scraper

---
+++ testpackage/pretendpackage.go
@@ -0,0 +1,9 @@
+//package testpackage has a package level comment up here
+package testpackage
+
+import "fmt"
+
+// ExportedFunction has a comment right here
+func ExportedFunction() {
+	fmt.Println("vim-go")
+}
